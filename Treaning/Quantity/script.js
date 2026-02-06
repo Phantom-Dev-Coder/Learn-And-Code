d73ba94ceaf00af36cf010363719fb45ae404a64
Learn And Code <mrmahdi.dev@protonmail.com>
2026-02-06 14:16:21 +0330

    after merge

--- Treaning/Quantity/script.js
+++ Treaning/Quantity/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let Quantity = 0 ;
 
 function Add1 (){
@@ -20,27 +19,4 @@ function ShowQuantity (){
 function Reset (){
   Quantity = 0
   alert(`Quantity =0 `)
-=======
-let Quantity = 0 ;
-
-function Add1 (){
-  Quantity += 1
-}
-
-function Add2 (){
-  Quantity += 2
-}
-
-function Add3 (){
-  Quantity += 3
-}
-
-function ShowQuantity (){
-  alert(Quantity)
-}
-
-function Reset (){
-  Quantity = 0
-  alert(`Quantity =0 `)
->>>>>>> 74f9fd5f165044ef486b804e8132dd11c2dcf73a
 }
--- Treaning/Todo list/script.js
+++ Treaning/Todo list/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Array for todo list !
 const TodoList = [{
   TodoText : 'Macke Dinner',
@@ -67,74 +66,4 @@ function TackeValue () {
   InputTime.value = '';
   //show list in web !
   Render();
-=======
-//Array for todo list !
-const TodoList = [{
-  TodoText : 'Macke Dinner',
-  TodoDate : '2026-01-15',
-  TodoTime : '21:06'
-},{
-  TodoText : 'Macke Dinner',
-  TodoDate : '2026-01-15',
-  TodoTime : '21:06'
-},{
-  TodoText : 'Macke Dinner',
-  TodoDate : '2026-01-15',
-  TodoTime : '21:06'
-}];
-
-Render();
-
-function Render () {
-  //save loop result !
-  let TodoListHTML = '';
-  //loop for adding result to web page !
-  for (let i = 0 ; i < TodoList.length ; i++) {
-    const TodoTextObjeckt = TodoList[i];
-    const {TodoText} = TodoTextObjeckt; 
-    const {TodoDate} = TodoTextObjeckt;
-    const {TodoTime} = TodoTextObjeckt;
-    const Num = i+1
-    const html = `
-    <div>${Num}</div>
-    <div><input type="checkbox" class="Input-check"></div>
-    <div class="list-text">${TodoText}</div>  
-    <div class="list-text">${TodoDate}</div>
-    <div class="list-text">${TodoTime}</div>
-    <button onclick="
-      TodoList.splice(${i} , 1);
-      Render();"
-      class="todo-item-delete">
-      Delete
-    </button>
-    `;
-    TodoListHTML += html;
-  };
-  document.querySelector('.todo-list').innerHTML = TodoListHTML;
-};
-
-//tacke the value from web page !
-function TackeValue () {
-  //acsses to deta from web page and save in to the vareubel !
-  const InputText = document.querySelector('.input-todo-js');
-  const TodoInputText = InputText.value;
-
-  const InputDate = document.querySelector('.input-todo-date-js');
-  const TodoInputDate = InputDate.value;
-  
-  const InputTime = document.querySelector('.input-todo-time-js');
-  const TodoInputTime = InputTime.value;
-  //update date for todo list objeckt from top !
-  TodoList.push({
-    TodoText: TodoInputText,
-    TodoDate: TodoInputDate,
-    TodoTime: TodoInputTime
-  }) ;
-  //clear the placsholder in web !
-  InputText.value = '';
-  InputDate.value = '';
-  InputTime.value = '';
-  //show list in web !
-  Render();
->>>>>>> 74f9fd5f165044ef486b804e8132dd11c2dcf73a
 };
